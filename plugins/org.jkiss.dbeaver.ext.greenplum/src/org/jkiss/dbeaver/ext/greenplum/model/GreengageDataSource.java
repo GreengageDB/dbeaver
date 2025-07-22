4297a0690d92e1ff4dc439eb2b33c1e1ca84b826
avv <avv@arenadata.io>
2025-07-22 11:08:10 +0300

    #38810 Add Greengage Database support

---
+++ plugins/org.jkiss.dbeaver.ext.greenplum/src/org/jkiss/dbeaver/ext/greenplum/model/GreengageDataSource.java
@@ -0,0 +1,70 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.dbeaver.ext.greenplum.model;
+
+import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.DBException;
+import org.jkiss.dbeaver.Log;
+import org.jkiss.dbeaver.model.DBPDataSourceContainer;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.meta.Association;
+import org.jkiss.dbeaver.ext.postgresql.PostgreUtils;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
+import org.osgi.framework.Version;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GreengageDataSource
+        extends GreenplumDataSource {
+
+    private static final Log log = Log.getLog(GreengageDataSource.class);
+
+    private Boolean hasAccessToExttable;
+
+    public GreengageDataSource(DBRProgressMonitor monitor, DBPDataSourceContainer container) throws DBException {
+        super(monitor, container);
+    }
+
+    @Override
+    public void initialize(@NotNull DBRProgressMonitor monitor) throws DBException {
+        super.initialize(monitor);
+        try (JDBCSession session = DBUtils.openMetaSession(monitor, this, "Read Greengage server special info")) {
+            if (serverVersion != null) {
+                Matcher matcher = Pattern.compile("Greengage Database ([0-9\\.]+)").matcher(serverVersion);
+                if (matcher.find()) {
+                    gpVersion = new Version(matcher.group(1));
+                }
+            }
+            if (hasAccessToExttable == null) {
+                hasAccessToExttable = PostgreUtils.isMetaObjectExists(session, "pg_exttable", "*");
+            }
+
+        } catch (Throwable e) {
+            log.debug("Error reading GG server version", e);
+        }
+        if (gpVersion == null) {
+            gpVersion = new Version(4, 2, 0);
+        }
+    }
+
+    @Association
+    public boolean supportsExternalTables() {
+        return hasAccessToExttable;
+    }
+}
